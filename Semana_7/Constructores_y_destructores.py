4900301b9eec2651b0b94f58612a2f858c1f4421
Lucía BravoUEA <lc.bravoo@uea.edu.ec>
2025-02-21 14:52:25 -0500

    semana 7

--- Semana_7/Constructores_y_destructores.py
+++ Semana_7/Constructores_y_destructores.py
@@ -4,10 +4,10 @@ class Lápiz:
     def __init__(self, marca, color):
         self.marca = marca
         self.color = color
-        print("El método decontructor se ha ejecutadocon éxito")
+        print("El método  contructor se ha ejecutadocon éxito")
 
     def datos(self):
-        print(f"tu esfero es marca {self.marca} y color: {self.color}")
+        print(f"Tu Lápiz es de  marca {self.marca} y color: {self.color}")
 
     #Metodo Destructor
     def __del__(self):
@@ -17,10 +17,10 @@ class Esfero:
     def __init__(self, marca, color):
         self.marca = marca
         self.color = color
-        print("Método de la clse Esfero iniciado")
+        print("Método de la clase Esfero iniciado")
 
     def datos(self):
-        print(f"tu Esfero es marca {self.marca} y color {self.color}")
+        print(f"Tu Esfero es de marca {self.marca} y color {self.color}")
 
     def __del__(self):
         print("El método destructor de la clase Esfero se ha ejecutado")
